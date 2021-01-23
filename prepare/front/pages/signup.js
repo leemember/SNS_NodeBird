8799aef9c01c7b5c3ea0bb7bdb88c98a41ab73f8
leehyunju <hyunmail521@gmail.com>
2021-01-23 17:05:48 +0900

    중복되는 hooks들

--- prepare/front/pages/signup.js
+++ prepare/front/pages/signup.js
@@ -1,8 +1,28 @@
-import React from 'react'; 
+import React, { useCallback, useState } from 'react'; 
 import Head from 'next/head';
+import { Form, Input } from 'antd';
 import AppLayout from '../components/AppLayout';
 
 const Signup = () => {
+    const [id, setId] = useState('');
+    const onChangeId = useCallback((e) => {
+        setId(e.target.value);
+    }, []);
+
+    const [nickname, setNickname] = useState('');
+    const onChangeNickname = useCallback((e) => {
+        setNickname(e.target.value);
+    }, []);
+
+    const [password, setPassword] = useState('');
+    const onChangePassword = useCallback((e) => {
+        setPassword(e.target.value);
+    }, []);
+
+    const onSubmit = useCallback(() => {
+
+    }, []);
+
     return (
         <>
         <Head>
@@ -10,7 +30,36 @@ const Signup = () => {
             <title>회원가입 | NodeBird</title>
         </Head>
 
-        <AppLayout>회원가입 페이지</AppLayout>
+        <AppLayout>
+            <Form onFinish={onFinish}>
+                <div>
+                    <label htmlFor="user-id">아이디</label>
+                    <br />
+                    <Input name="user-id" value={id} required onChange={onChangeId} />
+                </div>
+                <div>
+                    <label htmlFor="user-nick">닉네임</label>
+                    <br />
+                    <Input name="user-nick" value={nickname} required onChange={onChangeNickname} />
+                </div>
+                <div>
+                    <label htmlFor="user-password">비밀번호</label>
+                    <br />
+                    <Input name="user-password" value={password} required onChange={onChangePassword} />
+                </div>
+                <div>
+                    <label htmlFor="user-password-check">비밀번호체크</label>
+                    <br />
+                    <Input
+                        name="user-password-check"
+                        type="password"
+                        value={passwordCheck}
+                        required
+                        onChange={onChangePasswordCheck}
+                    />
+                </div>
+            </Form>
+        </AppLayout>
       </>        
     )
 };
