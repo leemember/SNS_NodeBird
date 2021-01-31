4810688c25afb6a11a9845de08113dc5bb9c904d
이현주 <leehyunju@ihyeonjuui-MacBookAir.local>
2021-01-31 20:38:27 +0900

    터미널창에 뜨는 거슬리는 문구 없애기, 코드 공백 없애기

--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -19,9 +19,9 @@ const configureStore = () => {
   const sagaMiddleware = createSagaMiddleware(); 
   const middlewares = [sagaMiddleware, loggerMiddleware];
   const enhancer = process.env.NODE_ENV === 'production'
-      ? compose(applyMiddleware(...middlewares)) // 배포용일 때는 devTool 연결 안함
-      : composeWithDevTools(applyMiddleware(...middlewares))
-      // 개발용일 때는 composeWithDevTools로 바꿔주면 된다.
+    ? compose(applyMiddleware(...middlewares)) // 배포용일 때는 devTool 연결 안함
+    : composeWithDevTools(applyMiddleware(...middlewares))
+    // 개발용일 때는 composeWithDevTools로 바꿔주면 된다.
   const store = createStore(reducer, enhancer);   
   store.sagaTask = sagaMiddleware.run(rootSaga);
   return store;
@@ -30,7 +30,8 @@ const configureStore = () => {
 
 //두번재는 옵션 객체이다.
 const wrapper = createWrapper(configureStore, { 
-    debug: process.env.NODE_ENV === 'development',
+  debug: false,
+  //process.env.NODE_ENV === 'development',
 });
 
 export default wrapper;
