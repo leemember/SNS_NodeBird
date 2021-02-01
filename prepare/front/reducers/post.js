38ff21c5f6a14dfed7e19cfbf20e0567d8c25925
이현주 <leehyunju@ihyeonjuui-MacBookAir.local>
2021-02-01 10:21:14 +0900

    타입생성함수 만듬

--- prepare/front/reducers/post.js
+++ prepare/front/reducers/post.js
@@ -1,65 +1,68 @@
 export const initialState = {
-    mainPosts: [{
-        //이 부분은 프론트가 백엔드한테 어떤식으로 개발 한건지 물어보고 협업하는 것이 좋다.
-        id: 1,
-        User: {
-          id: 1,
-          nickname: '현주리',
-        },
-        content: '첫 번째 게시글 #해시태그 #익스프레스',
-        Images: [
-        {src: 'https://usercontents-c.styleshare.io/images/38020339/640x-',},
-        {src: 'https://cdn.fanzeel.com/images/201906/5cff5ddc67935.jpg',},
-        {src: 'https://t1.daumcdn.net/cfile/tistory/1122D22F4C691BB395',},
-    ],
-    Comments: [{
-        User: {
-            nickname: 'hyunju'
-        },
-        content: '우와 재밌겠군요~',
-    }, {
-        User: {
-            nickname: 'lee',
-        },
-        content: '멋있네요'
-    }]
-    //왜 id랑 conent는 앞에 글자가 소문자인데 그 나머지들은 왜 대문자냐면 이것은 시퀄라이즈와 연관이 있다.
-    // 다른 정보와 관련이 있는 데이터면 그 정보를 한 번에 합쳐준다. 합쳐줄 때는 대문자가 되어 나온다.
-    }],
-    imagePaths: [], // 이미지 업로드 할 때 이미지 경로들이 저장되는 곳이다.
-    postAdded: false, // 게시될 것들이 추가가 완료됐을 때
-
+  mainPosts: [{
+    //이 부분은 프론트가 백엔드한테 어떤식으로 개발 한건지 물어보고 협업하는 것이 좋다.
+    id: 1,
+    User: {
+      id: 1,
+      nickname: '현주리',
+    },
+    content: '첫 번째 게시글 #해시태그 #익스프레스',
+    Images: [
+      {src: 'https://usercontents-c.styleshare.io/images/38020339/640x-',},
+      {src: 'https://cdn.fanzeel.com/images/201906/5cff5ddc67935.jpg',},
+      {src: 'https://t1.daumcdn.net/cfile/tistory/1122D22F4C691BB395',},
+  ],
+  Comments: [{
+    User: {
+        nickname: 'hyunju'
+    },
+    content: '우와 재밌겠군요~',
+  }, {
+    User: {
+        nickname: 'lee',
+    },
+    content: '멋있네요'
+  }]
+  //왜 id랑 conent는 앞에 글자가 소문자인데 그 나머지들은 왜 대문자냐면 이것은 시퀄라이즈와 연관이 있다.
+  // 다른 정보와 관련이 있는 데이터면 그 정보를 한 번에 합쳐준다. 합쳐줄 때는 대문자가 되어 나온다.
+  }],
+  imagePaths: [], // 이미지 업로드 할 때 이미지 경로들이 저장되는 곳이다.
+  postAdded: false, // 게시될 것들이 추가가 완료됐을 때
 }
 
-const ADD_POST = 'ADD_POST';
+export const ADD_POST_REQUEST = 'ADD_POST_REQUEST';
+export const ADD_POST_SUCCESS = 'ADD_POST_SUCCESS';
+export const ADD_POST_FAILURE = 'ADD_POST_FAILURE';
+
+
 // 액션 이름을 상수로 빼주면 좋은 것이 타입과 케이스에 그대로 재활용 할 수 있다. 중간에 오타날 경우를 예방 함.
 export const addPost = {
     type: ADD_POST,
 }
 
 const dummyPost = {
-    id:2,
-    content: '더미 데이터입니다.',
-    User: {
-        id: 1,
-        nickname:'현주리',
-    },
-    Images: [],
-    Comments: [],
+  id:2,
+  content: '더미 데이터입니다.',
+  User: {
+    id: 1,
+    nickname:'현주리',
+  },
+  Images: [],
+  Comments: [],
 };
 
 const reducer = (state = initialState, action ) => {
-    switch (action.type) {
-        case ADD_POST:
-            return {
-                ...state,
-                mainPosts: [dummyPost, ...state.mainPosts],
-                //여기서 dummyPost이 함수는 앞에다 추가해야 게시물을 추가했을 때 가장 위에 뜬다.
-                postAdded: true
-            }
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case ADD_POST:
+      return {
+        ...state,
+        mainPosts: [dummyPost, ...state.mainPosts],
+        //여기서 dummyPost이 함수는 앞에다 추가해야 게시물을 추가했을 때 가장 위에 뜬다.
+        postAdded: true
+      }
+      default:
+        return state;
+  }
 }
 
 export default reducer;
--- prepare/front/reducers/user.js
+++ prepare/front/reducers/user.js
@@ -1,66 +1,97 @@
 export const initialState = {
-  isLoggingIn: false, // 로그인 시도중
-  isLoggedIn: false,
-  isLoggingOut: false, // 로그아웃 시도중
+  logInLoading : false, // 로그인 시도중
+  logInDone : false,
+  logInError : null,
+  logOutLoading : false, // 로그아웃 시도중
+  logOutDone : false,
+  logOutFailure : null,
+  signUpLoading : false, // 회원가입 시도중
+  signUpDone : false,
+  signUpFailure : null,
+  
   me: null,
   signUpData: {},
   loginData: {},
 }
 
+export const LOG_IN_REQUEST = 'LOG_IN_REQUEST';
+export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
+export const LOG_IN_FAILURE = 'LOG_IN_FAILURE';
+
+export const LOG_OUT_REQUEST = 'LOG_OUT_REQUEST';
+export const LOG_OUT_SUCCESS = 'LOG_OUT_SUCCESS';
+export const LOG_OUT_FAILURE = 'LOG_OUT_FAILURE';
+
+export const SING_UP_REQUEST = 'SING_UP_REQUEST';
+export const SING_UP_SUCCESS = 'SING_UP_SUCCESS';
+export const SING_UP_FAILURE = 'SING_UP_FAILURE';
+
+export const FOLLOW_REQUEST = 'FOLLOW_REQUEST';
+export const FOLLOW_SUCCESS = 'FOLLOW_SUCCESS';
+export const FOLLOW_FAILURE = 'FOLLOW_FAILURE';
+
+export const UNFOLLOW_REQUEST = 'UNFOLLOW_REQUEST';
+export const UNFOLLOW_SUCCESS = 'UNFOLLOW_SUCCESS';
+export const UNFOLLOW_FAILURE = 'UNFOLLOW_FAILURE';
+
 //어떤 요청이든 이렇게 세 가지 함수가 나온다. *요청 / 성공 / 실패
 // LOGIN-----------------------------------------
 export const loginRequestAction = (data) => {
   return {
-    type: 'LOG_IN_REQUEST',
+    type: LOG_IN_REQUEST,
     data,
   }
 }
 
 export const logoutRequestAction = () => {
   return {
-    type: 'LOG_OUT_REQUEST'
+    type: LOG_OUT_REQUEST
     // 로그아웃은 데이터가 필요 없으니 DATA를 없애도 된다.
   }
 }
 
 const reducer = (state = initialState, action ) => {
 switch (action.type) {
+
   //로그인
-  case 'LOG_IN_REQUEST':
-    console.log('리듀서 login');
+  case LOG_IN_REQUEST:
     return {
         ...state,
-        isLoggingIn:true,
-    }
-  case 'LOG_IN_SUCCESS':
+        logInLoading:true,
+        logInError:null,
+        logInDone: false
+    };
+
+  case LOG_IN_SUCCESS:
     return {
       ...state,
-      isLoggingIn: false,
-      isLoggedIn:true,
-      me: {...action.data, nickname: 'hyunjulee'},
-    }
-  case 'LOG_IN_FAILURE':
+      logInLoading: false,
+      logInDone:true,
+      me: {...action.data, nickname: 'hyunjulee'}, //더미데이터
+    };
+
+  case LOG_IN_FAILURE:
     return {
       ...state,
-      isLoggingIn: false,
-      isLoggedIn:false,
+      logInLoading: false,
+      logInError:false,
     }
 
   //----------------------------
   //로그아웃
-  case 'LOG_OUT_REQUEST':
+  case LOG_OUT_REQUEST:
     return {
       ...state,
-      isLoggingOut:false,
+      isLoggingOut:true,
     }
-  case 'LOG_OUT_SUCCESS':
+  case LOG_OUT_SUCCESS:
     return {
       ...state,
       isLoggingOut:false,
       isLoggedIn:false,
       me: null
     }
-  case 'LOG_OUT_FAILURE':
+  case LOG_OUT_FAILURE:
     return {
       ...state,
       isLoggingOut:false, // 요청이 끝났으니까 false
